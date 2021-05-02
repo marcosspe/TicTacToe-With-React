829730c44b6ba917cd9bb03f9b8320bd12cc79c9
Gutclara <mariadelasclaras@gmail.com>
2021-05-02 19:04:44 +0000

    tablero que le falta onda

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,8 +7,27 @@ import rigoImage from "../../img/rigo-baby.jpg";
 export function Home() {
 	return (
 		<div>
-			<h1 className="display-3 text-center">Ta Te Ti</h1>
-			<div className="contenedor col-12 d-flex"></div>
+			<h1 className="display-3 text-center">Tic Tac Toe</h1>
+			<button type="button" className="btn btn-light" id="startButton">
+				START
+			</button>
+			<div className="contenedor col-12">
+				<div className="row">
+					<div className="cell">Hola</div>
+					<div className="cell"></div>
+					<div className="cell"></div>
+				</div>
+				<div className="row">
+					<div className="cell"></div>
+					<div className="cell"></div>
+					<div className="cell"></div>
+				</div>
+				<div className="row">
+					<div className="cell"></div>
+					<div className="cell"></div>
+					<div className="cell"></div>
+				</div>
+			</div>
 		</div>
 	);
 }
