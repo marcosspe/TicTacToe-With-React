38bc7aa8ebc0d7d6b52af7a4ea46fffcf4ca3724
marcosspe <marcos.perez.garcia0@gmail.com>
2021-05-05 19:08:36 +0000

    marcos

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,15 +5,15 @@ import { Tablero } from "./tablero.js";
 //create your first component
 export function Home() {
 	const [inicio, setInicio] = useState(true);
+	var setin = setInicio;
 
 	return (
 		<div>
 			<h1 className="text-white">Tic Tac Toe in React.js</h1>
 			{inicio ? <Login /> : <Tablero />}
-
 			<button
 				id="start"
-				className="rounded btn-light mt-3"
+				className="start rounded btn-light mt-3"
 				onClick={() => {
 					setInicio(false);
 				}}>
